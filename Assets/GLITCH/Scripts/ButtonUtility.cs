e5f929400041287a08b6e2f78895aa581cc25129
GlitchBoss <theprograminator@gmail.com>
2016-03-17 14:41:41 -0400

    Added Various Functions
    Added Miscellaneous Script (will probably be renamed)
    Added SendMessageToAll Function to Miscellaneous
    Added StartGame, PauseGame, ResumeGame, and ChangeTimeSpeed to
    ButtonUtility

--- Assets/GLITCH/Scripts/ButtonUtility.cs
+++ Assets/GLITCH/Scripts/ButtonUtility.cs
@@ -1,9 +1,34 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 namespace GLITCH.Utilities
 {
 	public class ButtonUtility : MonoBehaviour {
 
+		public static void StartGame()
+		{
+			Time.timeScale = 1.0f;
+			Time.fixedDeltaTime = 0.02f;
+			Miscelanious.SendMessageToAll("StartGame");
+		}
+
+		public static void PauseGame()
+		{
+			Time.timeScale = 0;
+			Time.fixedDeltaTime = 0.0f;
+			Miscelanious.SendMessageToAll("PauseGame");
+		}
+
+		public static void ResumeGame()
+		{
+			Time.timeScale = 1.0f;
+			Time.fixedDeltaTime = 0.02f;
+			Miscelanious.SendMessageToAll("ResumeGame");
+		}
+
+		public static void ChangeTimeSpeed(float newSpeed)
+		{
+			Time.timeScale = newSpeed;
+			Time.fixedDeltaTime = 0.02f;
+		}
 	}
 }
---
+++ Assets/GLITCH/Scripts/Miscellaneous.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace GLITCH.Utilities
+{
+	public class Miscelanious : MonoBehaviour {
+		
+		public static void SendMessageToAll(string message)
+		{
+			GameObject[] gos = FindObjectsOfType<GameObject>();
+			foreach (GameObject go in gos)
+			{
+				go.BroadcastMessage(message, SendMessageOptions.DontRequireReceiver);
+			}
+		}
+
+	}
+}
