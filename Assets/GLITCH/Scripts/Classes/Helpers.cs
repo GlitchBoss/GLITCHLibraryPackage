06ef96db7e72829ac1be143b2ff62c1447827a13
GlitchBoss <theprograminator@gmail.com>
2016-03-18 17:13:54 -0400

    Added Helpers

---
+++ Assets/GLITCH/Scripts/Classes/Helpers.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace GLITCH.Helpers
+{
+	public class Helpers
+	{
+
+	}
+
+	[Serializable]
+	public class RangeInt
+	{
+		public int max;
+		public int min = 0;
+		public int num;
+	}
+
+	[Serializable]
+	public class RangeFloat
+	{
+
+		public float max;
+		public float min = 0;
+		public float num;
+	}
+
+	[Serializable]
+	public class RandomInt
+	{
+		public int min, max;
+		public int random
+		{
+			get { return UnityEngine.Random.Range(min, max); }
+		}
+	}
+
+	[Serializable]
+	public class RandomFloat
+	{
+		public float min, max;
+		public float random
+		{
+			get { return UnityEngine.Random.Range(min, max); }
+		}
+	}
+}
