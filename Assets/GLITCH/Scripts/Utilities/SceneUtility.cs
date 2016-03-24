3213ecca7df36ce5b39cc9215e8281bb228b5e0b
GlitchBoss <theprograminator@gmail.com>
2016-03-24 07:14:35 -0400

    Added SceneManager

---
+++ Assets/GLITCH/Scripts/Utilities/SceneUtility.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+namespace GLITCH.Utilities
+{
+	public class SceneUtility
+	{
+		private static List<GameObject> dontDestroy = new List<GameObject>();
+
+		public static void LoadScene(Scene scene)
+		{
+			foreach (GameObject go in dontDestroy.ToArray())
+			{
+				Object.DontDestroyOnLoad(go);
+			}
+			SceneManager.LoadScene(scene.name);
+			foreach (GameObject go in dontDestroy)
+			{
+				SceneManager.MoveGameObjectToScene(go, scene);
+				go.SendMessage("StartUp");
+			}
+		}
+
+		public static void LoadScene(int build)
+		{
+			foreach (GameObject go in dontDestroy.ToArray())
+			{
+				Object.DontDestroyOnLoad(go);
+			}
+			SceneManager.LoadScene(build);
+			foreach (GameObject go in dontDestroy)
+			{
+				SceneManager.MoveGameObjectToScene(go, SceneManager.GetSceneAt(build));
+				go.SendMessage("StartUp");
+			}
+		}
+
+		public static void LoadScene(string name)
+		{
+			foreach (GameObject go in dontDestroy.ToArray())
+			{
+				Object.DontDestroyOnLoad(go);
+			}
+			SceneManager.LoadScene(name);
+			foreach (GameObject go in dontDestroy)
+			{
+				SceneManager.MoveGameObjectToScene(go, SceneManager.GetSceneByName(name));
+				go.SendMessage("StartUp");
+			}
+		}
+
+		public static void DontDestroyOnLoad(GameObject go)
+		{
+			dontDestroy.Add(go);
+		}
+	}
+}
